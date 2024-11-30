eb62be70a2b41e0fc6ebd9a9f280f5cb649219a1
suknum <guftj998@gmail.com>
2024-11-30 22:25:22 +0900

    git 업로드 연습

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println("==프로그램시작==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		String str = sc.nextLine();
+//		int cmd = sc.nextInt();
+//		sc.nextInt();
+//		sc.next();
+		String cmd = sc.next();
+		System.out.println("[ " + cmd + " ]");
+		
+		System.out.println("==프로그램끝==");
+	}
+}
